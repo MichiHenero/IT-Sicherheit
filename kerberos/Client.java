054f656dbf06a2e2e65d54701589ececbfb6533f
Michi Henero <henero@gmx.de>
2016-01-16 14:38:48 +0100

    Client login

--- kerberos/Client.java
+++ kerberos/Client.java
@@ -13,6 +13,8 @@ public class Client extends Object {
 	private String currentUser; // Speicherung bei Login nötig
 	private Ticket tgsTicket = null; // Speicherung bei Login nötig
 	private long tgsSessionKey; // K(C,TGS) // Speicherung bei Login nötig
+	
+	private long userPasswordKey; // K(C)
 
 	// Konstruktor
 	public Client(KDC kdc) {
@@ -21,22 +23,43 @@ public class Client extends Object {
 
 	public boolean login(String userName, char[] password) {
 		/* ToDo */
+		//User-Registrierung
+		myKDC.userRegistration(userName, password);		
+		
 		//Aufgabe: TGS-Ticket für den übergebenen Benutzer vom KDC (AS) holen (TGS-Servername: myTGS) 
 		TicketResponse ticketResponse = myKDC.requestTGSTicket(userName, "myTGS", generateNonce());
-		if(ticketResponse == null) {
-			System.out.println("Invalid ticketResponce");
+		
+		//User-Passwort erstellen
+		userPasswordKey = generateSimpleKeyFromPassword(password);
+		
+		//Ticket-Anfrage entschlüsseln
+		if(!ticketResponse.decrypt(userPasswordKey)) {
+			System.out.println("Ticket-Anfrage wurde schon entschluesselt, oder der Schluessel ist falsch!");
 			return false;
 		}
-		// und zusammen mit dem TGS-Sessionkey und dem UserNamen speichern
+		
+		//Überprüfung, ob überhaupt eine Ticket-Anfrage zurück kommt
+		if(ticketResponse == null) {
+			System.out.println("Ticketanfrage fehlgeschlagen");
+			return false;
+		}
+		
+		//und zusammen mit dem TGS-Sessionkey und dem UserNamen speichern aus der Ticket-Anfrage
 		currentUser = userName;
 		tgsTicket = ticketResponse.getResponseTicket();
-		tgsSessionKey = tgsTicket.getSessionKey();
+		tgsSessionKey = ticketResponse.getSessionKey();
 		
 		return true;	
 	}
 
 	public boolean showFile(Server fileServer, String filePath) {
 		/* ToDo */
+		//TGS-Ticket mit TGS-Session-Key verschlüsseln
+		if (tgsTicket.encrypt(tgsSessionKey)) {
+			myKDC.requestServerTicket(tgsTicket, tgsAuth, serverName, nonce)
+		}
+		
+		return true;
 	}
 
 	/* *********** Hilfsmethoden **************************** */
@@ -58,4 +81,15 @@ public class Client extends Object {
 		long rand = (long) (100000000 * Math.random());
 		return rand;
 	}
+	
+	public static void main(String[] args) {
+		
+		
+		
+		KDC kdc = new KDC("myTGS");
+		Client c = new Client(kdc);
+		char[] password = { 'p', 'w'};
+		c.login("User", password);		
+	}
+	
 }
